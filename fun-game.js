b22dad19ce1d5abcac402d1cda6c33491223deaa
Nodukishor <nipuchakraborty86@hotmail.com>
2020-08-13 00:16:23 +0600

    let const var

--- fun-game.js
+++
@@ -1,5 +0,0 @@
- function hashCode(s){
-    return window.btoa(s)            
- }
-
- console.log(hashCode("hdjkhsjhdkhskdhks"))
--- typeConversion.js
+++
@@ -1,8 +0,0 @@
-let number = 1;
-let convertIntegerToFloat=Math.floor(number)
-
-let convertNumberToString =number.toString()
-
-// console.log(typeof convertNumberToString)
-//  console.log(typeof convertIntegerToFloat)
-console.log(convertIntegerToFloat)
