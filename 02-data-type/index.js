91358910fd9e40f142af2c683551392ec518126e
Nodukishor <nipuchakraborty86@hotmail.com>
2020-08-13 01:09:43 +0600

    02-datatype

---
+++ 02-data-type/index.js
@@ -0,0 +1,41 @@
+/* 
+ Javascript has mainly two types of datatype and they are
+ 1. Primitive Datatype
+ 2. Reference Datatype
+
+ There 
+
+ what is primitive datatype?
+ Answer: In JavaScript, a primitive (primitive value, primitive data type)
+         is data that is not an object and has no methods. There are 
+         6 primitive data types: string, number, bigint, boolean, 
+         undefined, and symbol
+ What is Reference Datatype?
+ Answer: Simple we can What can access by Reference variable it's called reference type data type
+*/
+
+
+/* 
+let see example:
+*/
+
+let number = 1 // Number type
+
+//let's check it 
+
+console.log(typeof number)  //it will show you "number" as a output
+
+let stringValue = "This is a string" //sting data type
+
+//let's check it 
+
+console.log(typeof stringValue) //it will show you this output "string"
+
+let isActive = true; // it's a boolean datatype
+
+//let's check it
+
+console.log(typeof isActive) //it's show as a output "boolean"
+
+
+
