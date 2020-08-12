3f7959c56665bc86add668aa70b2aa0a295711d7
Nodukishor <nipuchakraborty86@hotmail.com>
2020-08-13 01:31:13 +0600

    02-datatype-complete

--- 02-data-type/index.js
+++ 02-data-type/index.js
@@ -37,5 +37,96 @@ let isActive = true; // it's a boolean datatype
 
 console.log(typeof isActive) //it's show as a output "boolean"
 
+let donot_know_what_should_be_here=null; //Null datatype
+
+console(typeof donot_know_what_should_be_here) //it's show you Object datatype because null is one kind of object
+
+
+let I_do_not_want_define_now=undefined; //undefine datatype
+
+console.log(typeof I_do_not_want_define_now) //it will show as output undefined
+
+
+/* 
+Now let see Reference datatype
+*/ 
+
+let person = [1,2,3,4,5];
+
+console.log(Array.isArray(person)) //it will show as a output true
+console.log(typeof person) //it will show you object because person is a one kind of object in js
+
+
+// let's see another example
+
+const persons=[
+    {
+        name:"Nipu",
+        age:23,
+        profession:"Programmer",
+        designation:"Fullstack developer"
+    },
+    {
+        name:"Fardin",
+        age:23,
+        profession:"Programmer",
+        designation:"Fullstack developer"
+    },
+    {
+        name:"Mr bin",
+        age:44,
+        profession:"Comedian",
+        designation:"Profession"
+    },
+];
+
+//this gonna call nested array object so let's check it
+
+console.log(typeof persons) //it'll show you as output Object
+console.log(Array.isArray(persons)) //it's will show you as true
+
+
+//let's deep into Object datatype
+
+
+let phone_book={
+  phone_number:"01827263486",
+  name:"John Hello",
+}
+
+console.log(typeof phone_book) //it will show you as a output "Object"
+
+//let's see how can we create a nested array
+
+let phone_books={
+    person1:{
+        phone_number:"01827263486",
+        name:{
+            firstName:"John",
+            lastName:"Doe"
+        } 
+    },
+    person2:{
+        phone_number:"01798306639",
+        name:{
+            firstName:"Nk",
+            lastName:"Chak"
+        } 
+    },
+}
+
+
+//let's check it
+
+console.log(typeof phone_books) //it's will show you as a output "Object"
+
+
+
+
+
+
+
+
+
 
 
